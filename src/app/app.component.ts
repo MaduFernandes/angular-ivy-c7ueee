d98afa8175d2f11658b1246fafba2f16c9ad709f
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2022-06-20 11:32:38 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IInstitutions } from './interface/institutions';
 import { InstitutionsService } from './services/institutions.service';
 
@@ -9,7 +9,8 @@ import { InstitutionsService } from './services/institutions.service';
 })
 export class AppComponent implements OnInit {
   institutions: IInstitutions[];
-  isChecked: boolean = false;
+  isChecked: boolean;
+  checkedInstitutions;
 
   constructor(private institutionService: InstitutionsService) {}
 
@@ -26,9 +27,31 @@ export class AppComponent implements OnInit {
   }
 
   checkUncheckedAll() {
-    console.log('entrei');
-    // for (var index = 0; index < this.institutions.length; index++) {
-    //   console.log(this.institutions[index].checked);
-    // }
+    for (var index = 0; index < this.institutions.length; index++) {
+      if (this.institutions[index].checked === true) {
+        this.institutions[index].checked = false;
+      } else {
+        this.institutions[index].checked = true;
+      }
+    }
+
+    this.getCheckedItemList();
+  }
+
+  isAllSelected() {
+    this.isChecked = this.institutions.every(function (item: any) {
+      return item.isSelected == true;
+    });
+
+    this.getCheckedItemList();
+  }
+
+  getCheckedItemList() {
+    this.checkedInstitutions = [];
+    for (var i = 0; i < this.institutions.length; i++) {
+      if (this.institutions[i].checked)
+        this.checkedInstitutions.push(this.institutions[i]);
+    }
+    this.checkedInstitutions = JSON.stringify(this.checkedInstitutions);
   }
 }
